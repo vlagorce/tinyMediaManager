91f9ca5ea4f7b178d23476838afe288fa39c9b7e
myron0815 <myron0815@gmx.net>
2017-10-29 11:06:08 +0100

    episodeguide cache value should also have a language in there!

--- src/main/java/org/tinymediamanager/core/tvshow/connector/TvShowToXbmcNfoConnector.java
+++ src/main/java/org/tinymediamanager/core/tvshow/connector/TvShowToXbmcNfoConnector.java
@@ -166,7 +166,7 @@ public class TvShowToXbmcNfoConnector {
     String tvdbid = tvShow.getIdAsString(Constants.TVDB);
     if (StringUtils.isNotBlank(tvdbid)) {
       xbmc.setId(tvdbid);
-      xbmc.episodeguide.url.cache = tvdbid + ".xml";
+      xbmc.episodeguide.url.cache = tvdbid + "-" + TvShowModuleManager.SETTINGS.getScraperLanguage().name() + ".xml";
       xbmc.episodeguide.url.url = "http://www.thetvdb.com/api/1D62F2F90030C444/series/" + tvdbid + "/all/"
           + TvShowModuleManager.SETTINGS.getScraperLanguage().name() + ".zip";
       // was XML, changed to ZIP

774411c7a72e479d5d96f62cff8468fc347454a1
Simon Legner <Simon.Legner@gmail.com>
2018-02-18 15:13:48 +0100

    Update copyright year in CLI, GUI

--- src/main/java/org/tinymediamanager/TinyMediaManager.java
+++ src/main/java/org/tinymediamanager/TinyMediaManager.java
@@ -158,7 +158,7 @@ public class TinyMediaManager {
     }
 
     LOGGER.info("=====================================================");
-    LOGGER.info("=== tinyMediaManager (c) 2012-2016 Manuel Laggner ===");
+    LOGGER.info("=== tinyMediaManager (c) 2012-2018 Manuel Laggner ===");
     LOGGER.info("=====================================================");
     LOGGER.info("tmm.version      : " + ReleaseInfo.getRealVersion());
 
